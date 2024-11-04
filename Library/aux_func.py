3f08c8687daaefae52fde24faddef3d4ed3f22b9
OlegUdalov <udalovog@gmail.com>
2024-11-04 18:50:26 -0500

    new codes
    new cods

--- Library/aux_func.py
+++ Library/aux_func.py
@@ -80,4 +80,50 @@ def plot_counts(counts, bit_size):
     
     plt.plot(xpoints, hyst)
     plt.show()
+
+def plot_counts_2(counts_1, counts_2, bit_size):
+    ''' the function plots number of counts for each state after measurements
+    IN contrast to previous function it plots 2 graph on the same plot for comparison
+    The input parameter is the counts object from the result.get_counts(cicr) function. This object is a dictionary with keys being the qubit states and the values corresponding to the key is the number of runs giving such an output. Note that the dictionary is not ordered and the key goes in some random order. 
+    The function order the outputs like 0000, 0001, 0010, 0011 etc. The the function just plot the counts as a function of state number.
+
+    Parameter:
+    __________
+    counts, dictionary with number of counts for each qubits state
+    bit_size, int, number of qubits in the circuit
+
+    
+    Returns:
+    __________
+    No returned value, just plot the hystpgram
+
+
+    '''
+    n_hyst = int(math.pow(2,bit_size))
+    hyst_1 = []
+    for i in range(n_hyst):
+        word = int_2_bin_word(i,bit_size)
+        if word in counts_1.keys():
+            hyst_1.append(counts_1[word])
+        else:
+            hyst_1.append(0)
+
+    hyst_2 = []
+    for i in range(n_hyst):
+        word = int_2_bin_word(i,bit_size)
+        if word in counts_2.keys():
+            hyst_2.append(counts_2[word])
+        else:
+            hyst_2.append(0)
+    
+    import matplotlib.pyplot as plt
+    import numpy as np
+    
+    xpoints = range(n_hyst)
+    
+    plt.plot(xpoints, hyst_1, label = ['1'])
+    plt.plot(xpoints, hyst_2, label = ['2'])
+    plt.legend()
+    plt.show()
+        
         
